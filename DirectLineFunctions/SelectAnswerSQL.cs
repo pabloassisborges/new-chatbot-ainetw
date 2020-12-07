a65b95d710a57546b27f3077d620623b4fb3d95f
Bia Kool <beatriz.ekool@outlook.com>
2020-12-07 10:59:12 -0300

    a

--- DirectLineFunctions/SelectAnswerSQL.cs
+++ DirectLineFunctions/SelectAnswerSQL.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -13,23 +14,46 @@ namespace BellaAPIs.Function
     public static class SelectAnswerSQL
     {
         [FunctionName("SelectAnswerSQL")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            string Intent = req.Query["Intent"];
+            // It must be enabled in the connection string.
+            string connectionString = GetConnectionString();
 
-            string name = req.Query["name"];
+            //SqlTransaction updateTx = null;
+            SqlCommand answerCmd = null;
+            //int answerID = 0;
+            string answer = null;
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string answerSQL =
+                "SELECT [Answer] FROM dbo.IntentAnswer ";// +
+                                                         //"WHERE [IntentName]"+ $" = @{Intent}";
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            using (SqlConnection awConnection = new SqlConnection(connectionString))
+            {
+                await awConnection.OpenAsync();
+                //updateTx = await Task.Run(() => awConnection.BeginTransaction());
+                answerCmd = new SqlCommand(answerSQL, awConnection);
+                //answerCmd.Transaction = updateTx;
+                SqlDataReader answerReader = answerCmd.ExecuteReader();// = await answerCmd.ExecuteReaderAsync();
+                while (await answerReader.ReadAsync())
+                {
+                    Console.WriteLine(answerReader["Answer"]);
+                    //answerID = (int)answerReader["ID"];
+                    answer = (string)answerReader["Answer"];
+                }
 
-            return new OkObjectResult(responseMessage);
+                string answerMessage = string.IsNullOrEmpty(answer)
+                    ? "Resposta não encontrada."
+                    : $"{answer}";
+
+                return new OkObjectResult(answerMessage);
+            }
+        }
+        private static string GetConnectionString()
+        {
+            //return "Data Source=(local);Integrated Security=SSPI;Initial Catalog=AdventureWorks;MultipleActiveResultSets=True";
+            return "Data Source= ainetworksprod.database.windows.net; Initial Catalog=BellaEduSuite; Pooling=True; Min Pool Size=25; Max Pool Size=500; Persist Security Info=False; User ID = amintas; Password = @3ki8YUjdj29!#F;";
         }
     }
 }
